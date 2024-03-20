0b1ab38dea406b8c8a61031866bade8a09c38841
chhavi5496jain <chhavi.jain@grazitti.com>
2024-03-20 12:10:35 +0530

    Update demo.js

--- blocks/demo/demo.js
+++ blocks/demo/demo.js
@@ -1,4 +1,8 @@
 export default function decorate(block) {
-  const customClass = block.children[2];
-  customClass.classList.add("hero-video-banner");
+  if (block && block.children && block.children.length >= 3) {
+    const customClass = block.children[2];
+    customClass.classList.add("hero-video-banner");
+  } else {
+    console.error("Invalid block or insufficient children.");
+  }
 }
