6014ed3cfd18356b37be28a1e2b564d52d90dba5
chhavi5496jain <chhavi.jain@grazitti.com>
2024-03-22 15:48:35 +0530

    Update demo.js

--- blocks/demo/demo.js
+++ blocks/demo/demo.js
@@ -2,13 +2,15 @@
 //   const customClass = section.children[0];
 //   customClass.classList.add("hero-video-banner");
 // }
-window.addEventListener("scroll", function () {
-  var header = document.getElementsByClassName("header-wrapper");
-  var firstElement = document.querySelector(".section-video"); // Assuming the first element has class 'content'
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    var header = document.getElementsByClassName("header-wrapper");
+    var firstElement = document.querySelector(".section-video"); // Assuming the first element has class 'content'
 
-  if (window.scrollY > firstElement.offsetTop) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
+    if (window.scrollY > firstElement.offsetTop) {
+      header.classList.add("scrolled");
+    } else {
+      header.classList.remove("scrolled");
+    }
+  });
 });
