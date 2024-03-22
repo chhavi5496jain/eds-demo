2cec043d9c240f09afe146f7af20b492929c8308
chhavi5496jain <chhavi.jain@grazitti.com>
2024-03-22 09:50:01 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -29,46 +29,4 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) header.append(fragment.firstElementChild);
 
   block.append(header);
-
-  // Create dropdown elements
-  const dropdownContainer = document.createElement("div");
-  dropdownContainer.classList.add("dropdown-container");
-
-  const dropdownButton = document.createElement("button");
-  dropdownButton.textContent = "Dropdown";
-  dropdownButton.classList.add("dropdown-button");
-
-  const dropdownContent = document.createElement("div");
-  dropdownContent.classList.add("dropdown-content");
-
-  const option1 = document.createElement("a");
-  option1.textContent = "Option 1";
-  option1.href = "#";
-
-  const option2 = document.createElement("a");
-  option2.textContent = "Option 2";
-  option2.href = "#";
-
-  dropdownContent.appendChild(option1);
-  dropdownContent.appendChild(option2);
-
-  dropdownContainer.appendChild(dropdownButton);
-  dropdownContainer.appendChild(dropdownContent);
-
-  // Append dropdown to the body
-  document.body.appendChild(dropdownContainer);
-
-  // Toggle dropdown menu
-  dropdownButton.addEventListener("click", function () {
-    dropdownContent.classList.toggle("show");
-  });
-
-  // Close dropdown menu if user clicks outside of it
-  window.addEventListener("click", function (event) {
-    if (!event.target.matches(".dropdown-button")) {
-      if (dropdownContent.classList.contains("show")) {
-        dropdownContent.classList.remove("show");
-      }
-    }
-  });
 }
